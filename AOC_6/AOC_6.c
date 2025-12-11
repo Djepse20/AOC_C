3c1b34da94bf39af65986d32c29794a8a662b769
daniel dencker <djepse20@student.aau.dk>
2025-12-11 22:59:32 +0100

    removed .txt files from ALL commits

--- AOC_6/AOC_6.c
+++ AOC_6/AOC_6.c
@@ -4,6 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct NumberCol {
+  uint64_t *numbers;
+  char op;
+  size_t len; // the hight, but lets call it len.
+} NumberCol;
+
+typedef struct NumberCols {
+  NumberCol *cols;
+  size_t len; // the width, but lets call it len.
+} NumberCols;
+
+void number_row_add(NumberCol *col, uint64_t num) {}
+
+int is_op(char ch) { return: }
+
 int main(int argc, char **argv) {
   FILE *aoc = open_file_from_args(argc, argv);
   char *lines = read_file(aoc);
